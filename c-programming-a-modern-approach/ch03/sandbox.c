06c331166c6a80b3562e0200ab0099a0e5a616aa
nrizzo64 <nicholas.rizzo64@proton.me>
2023-11-10 11:22:36 -0500

    Restructure directory. Add partially completed chapter 3 exercises and projects

---
+++ c-programming-a-modern-approach/ch03/sandbox.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main(void) {
+/*	printf("%6d,%4d\n", 86, 10400);
+	printf("%12.5e\n", 30.253);
+	printf("%.4f\n", 83.162);
+	printf("%-6.2g", .0000009979); 
+*/
+/* 2
+	float x = 8.06;
+	printf("%-8.1e", x);
+	printf("%10.6e", x);
+	printf("%-8.3f", x);
+	printf("%6.0f", x);
+*/
+/* 3
+	int d = 5;
+	float f = 8.64;
+	// a;
+	printf("%d\n", d);
+	printf(" %d\n", d);	
+	printf("\n");
+	// b;
+	printf("%d-%d-%d\n", d,d,d);
+	printf("%d -%d -%d\n", d,d,d);
+	printf("\n");
+	// c;
+	printf("%f\n", f);
+	printf("%f \n", f);
+	printf("\n");
+	// d;
+	printf("%f,%f\n", f, f);
+	printf("%f, %f\n", f, f);
+	printf("\n");
+	*/ 
+
+// 4.
+	/*
+	int i, j;
+	float x;
+	scanf("%d%f%d", &i, &x, &j);
+	printf("%d, %f, %d", i, x, j);
+	*/
+// 5.
+	float x, y;
+	int i;
+	scanf("%f%d%f", &x, &i, &y);
+	printf("%f, %d, %f", x, i, y);
+
+	return 0;
+}
